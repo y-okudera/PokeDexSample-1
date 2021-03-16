04d9ce810a616a1070b66a0bbfb796df5dc06f7d
okudera <appledev.yuoku@gmail.com>
2021-03-16 19:58:08 +0900

    org.koin.core.error.NoBeanDefFoundException: No definition found for 'com.example.home.HomeViewModel' has been found. Check your module definitions.の修正

--- app/src/main/java/com/example/pokedexsample/di/ViewModelModule.kt
+++ app/src/main/java/com/example/pokedexsample/di/ViewModelModule.kt
@@ -1,7 +1,10 @@
 package com.example.pokedexsample.di
 
 import org.koin.dsl.module
+import com.example.home.HomeViewModel
 
 val viewModelModule = module {
-
+    factory {
+        HomeViewModel()
+    }
 }
